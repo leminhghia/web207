6475b7b5f3f4d723b88768b3ebd46b1555c49da5
nghia00200 <nghialmpi00200@gmail.com>
2024-11-25 10:31:58 +0700

    feat: fix css

--- frontend/src/context/QuizContext.jsx
+++ frontend/src/context/QuizContext.jsx
@@ -1,19 +1,15 @@
 /* eslint-disable react/prop-types */
 import { createContext, useState } from "react";
-import { subjects, ADAV, ADTE, ADBS, ADUI, ASNE, CLCO, DBAV, DBBS, GAME, HTCS, INMA, JAAV, JABS, JSPR, LAYO, MOWE, PHPP, PMAG, VBPR, WEBU } from '../assets/assets'
-
-
-
-
 // eslint-disable-next-line react-refresh/only-export-components
 export const QuizContext = createContext();
+
 const QuizContextProvider = (props) => {
     const [reload, setReload] = useState(true)
-
-
+    const [visible, setVisible] = useState(false);//constext
+    const [userQuizId, setUserQuizId] = useState(null);//conteext
 
     const value = {
-        subjects, ADAV, ADTE, ADBS, ADUI, ASNE, CLCO, DBAV, DBBS, GAME, HTCS, INMA, JAAV, JABS, JSPR, LAYO, MOWE, PHPP, PMAG, VBPR, WEBU, reload, setReload
+        reload, setReload, visible, setVisible, userQuizId, setUserQuizId
     }
 
     return (
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import QuizContextProvider from './context/QuizContext.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <QuizContextProvider>
       <App />
+    </QuizContextProvider>
   </BrowserRouter>
 )
